2787e5836af29af90c6dafa3691818808ef67858
takumika <takumika@C010423114.zipangu.cac.co.jp>
2026-04-15 10:28:29 +0900

    lesson01.challenge07　完了

--- Java100_questions_cac2nd/src/lesson01/challenge07/Patisserie.java
+++ Java100_questions_cac2nd/src/lesson01/challenge07/Patisserie.java
@@ -41,6 +41,39 @@
 
 package lesson01.challenge07;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Patisserie {
+	public static void main(String[] args) throws IOException {
+		int sit = 30;
+		int sho = 30;
+		int pis = 30;
+
+		System.out.println("それぞれ何個ずつ買いますか？");
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		String str1 = reader.readLine();
+		double num1 = Double.parseDouble(str1);
+		String str2 = reader.readLine();
+		double num2 = Double.parseDouble(str2);
+		String str3 = reader.readLine();
+		double num3 = Double.parseDouble(str3);
+
+		System.out.println("シトロンを" + num1 + "個");
+		System.out.println("ショコラを" + num2 + "個");
+		System.out.println("ピスタチオを" + num3 + "個");
+
+		double sum = num1 + num2 + num3;
+
+		System.out.println("合計個数　　" + sum + "個");
+		System.out.println("合計金額　" + (num1 * 250 + num2 * 280 + num3 * 320) + "円");
+		System.out.println("をお買い上げですね。\n承りました。");
+		System.out.println("シトロン　　　\\250・・・残り" + (int) (sit - num1) + "個");
+		System.out.println("ショコラ　　　\\280・・・残り" + (int) (sho - num2) + "個");
+		System.out.println("ピスタチオ　　\\320・・・残り" + (int) (pis - num3) + "個");
+	}
 
 }
