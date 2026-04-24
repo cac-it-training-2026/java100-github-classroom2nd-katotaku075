86584855bdd386ad3a495e6ccfc8de2393b4e9da
takumika <takumika@C010423114.zipangu.cac.co.jp>
2026-04-24 17:01:10 +0900

    Lesson04.challenge13　完了

--- Java100_questions_cac2nd/src/lesson04/challenge13/WarehouseManager.java
+++ Java100_questions_cac2nd/src/lesson04/challenge13/WarehouseManager.java
@@ -63,32 +63,41 @@ public class WarehouseManager {
 
 	public static void main(String[] args) throws IOException {
 
-
 		//配列の宣言および初期化処理を記述する
-
+		String[] line = { "へい！らっしゃい！！", "何にしますか！"
+				+ "今日は活きのいいコハダが入ってますよ", "まいどあり！！またどうぞ" };
 
 		System.out.println("Yさん：");
 		System.out.println("まず、MISAKI○の今の状態を確かめようかな。\n");
 
-
 		//三つのあいさつパターンの出力処理を記述する
+		for (int i = 0; i < line.length; i++) {
+			System.out.println("あいさつパターン" + (i + 1) + "...「" + line[i] + "」");
 
+		}
 
 		System.out.println("Yさん：");
 		System.out.println("うわぁ～。どれか変更しよう。\n");
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-
 		//入力処理および入力値の代入処理
+		System.out.println("どのパターンを変更しますか＞");//挨拶パターンの選択
+		String str = br.readLine();
+		int element = Integer.parseInt(str);
 
+		System.out.println("セリフを吹き込んでください＞");//セリフ吹き込み
+		String InputLine = br.readLine();
+
+		line[element - 1] = InputLine;
 
 		System.out.println("\nYさん：");
 		System.out.println("よし。もう一度確かめてみよう。\n");
 
-
 		//三つのあいさつパターンの出力処理を記述する
-
+		for (int i = 0; i < line.length; i++) {
+			System.out.println("あいさつパターン" + (i + 1) + "...「" + line[i] + "」");
+		}
 
 		System.out.println("Yさん：");
 		System.out.println("まあ少しはマシになったかな。");
